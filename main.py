c803b715fee11260bda0a7dbaa48936d5a707565
iftahgideoni <iftah.gideoni@gmail.com>
2020-10-02 14:59:01 +0300

    arranges

--- main.py
+++ main.py
@@ -2,37 +2,9 @@ import xbee
 from json import loads, dumps
 from time import sleep
 import gc
-import machine
+# from umachine import UART
 
 COORDINATOR_64_BIT_ADDRESS = b'\x00\x13\xa2\x00A\xb7c\xae'
-
-
-class Gps:
-    def __init__(self, min, max, change, uart):
-        super().__init__(min, max, change)
-        self.uart = machine.UART(1, 9600)
-
-    def get_instance(self):
-        return self
-
-    def should_send(self, loop_idx, new_measure):
-        # return super().should_send(loop_idx, new_measure)
-        return True
-
-    def measure(self):
-        print("- Reading GPS data... ", end="")
-        self.uart.init(9600, bits=8, parity=None, stop=1)
-        sleep(1)
-        # Ensures that there will only be a print if the UART
-        # receives information from the GPS module.
-        while not self.uart.any():
-            if self.uart.any():
-                break
-        # Read data from the GPS.
-        gps_data = str(self.uart.read(), 'utf8')
-        # Close the UART.
-        self.uart.deinit()
-        return gps_data
 
 
 class Device:
@@ -57,7 +29,7 @@ class Device:
         # print("The XBee is %.1F degrees F" % (tp * 9.0 / 5.0 + 32.0))
         # print("The XBee is %.1F degrees C" % tp)
         self.COORD_64_ADDRESS = coord_64_address
-        self.health = {'is_ok': 'True', 'idx': 0}
+        self.health = {"is_ok": 'True', "idx": 0}
 
 
 class Sensor:
@@ -76,7 +48,7 @@ class Sensor:
         return (is_change_trigger and is_min_passed) or is_max_passed
 
 
-class XbeeTemperature(Sensor):
+class XbeeTemperatureSensor(Sensor):
     def __init__(self, min, max, change):
         super().__init__(min, max, change)
 
@@ -93,13 +65,41 @@ class XbeeTemperature(Sensor):
         return new_temp
 
 
+class Gps(Sensor):
+    def __init__(self, min, max, change):
+        super().__init__(min, max, change)
+        # self.uart = UART(1, 9600)
+
+    def get_instance(self):
+        return self
+
+    def should_send(self, loop_idx, new_measure):
+        # return super().should_send(loop_idx, new_measure)
+        return True
+
+    def measure(self):
+        print("- Reading GPS data... ", end="")
+        self.uart.init(9600, bits=8, parity=None, stop=1)
+        sleep(1)
+        # Ensures that there will only be a print if the UART
+        # receives information from the GPS module.
+        while not self.uart.any():
+            if self.uart.any():
+                break
+        # Read data from the GPS.
+        gps_data = str(self.uart.read(), 'utf8')
+        # Close the UART.
+        self.uart.deinit()
+        return gps_data
+
+
 gps_temp_device = Device(name="GPS_Temperature", coord_64_address=COORDINATOR_64_BIT_ADDRESS)
 # # ******* TRANSMIT BROADCAST ****************
 # #test_data = 'Hello World!'
 # #xbee.transmit(xbee.ADDR_BROADCAST,test_data)
-xbee_temperature = XbeeTemperature(10, 40, 0.2)
+xbee_temperature_sensor = XbeeTemperatureSensor(10, 40, 0.2)
 
-# uart_gps = Gps(u)
+#  uart_gps = Gps(5, 8, 0.1)
 
 print("Waiting for data...\n")
 
@@ -114,15 +114,19 @@ while True:
         print("Data received from %s >> %s" % (''.join('{:02x}'.format(x).upper() for x in sender), payload.decode()))
         try:
             received_data_dict = loads(payload.decode())
-            for item_key in received_data_dict.keys():
-                print("{0}: {1}".format(str(item_key), str(received_data_dict[item_key])))
+            # for item_key in received_data_dict.keys():
+            #     print("{0}: {1}".format(str(item_key), str(received_data_dict[item_key])))
             min_t = float(received_data_dict['min_interval'])
             max_t = float(received_data_dict['max_interval'])
             change_t = float(received_data_dict['change_threshold'])
-            xbee_temperature.min_interval = min_t
-            xbee_temperature.max_interval = max_t
-            xbee_temperature.change_trigger = change_t
-            print("updated: min={0}, max={1}, change_trigger={2}".format(str(xbee_temperature.min_interval), str(xbee_temperature.max_interval), str(xbee_temperature.change_trigger)))
+            xbee_temperature_sensor.min_interval = min_t
+            xbee_temperature_sensor.max_interval = max_t
+            xbee_temperature_sensor.change_trigger = change_t
+            xbee.transmit(gps_temp_device.COORD_64_ADDRESS,
+                          dumps({'Received params': {'min': int(xbee_temperature_sensor.min_interval),
+                                                     'max': int(xbee_temperature_sensor.max_interval),
+                                                     'change': xbee_temperature_sensor.change_trigger}}))
+            # print("updated: min={0}, max={1}, change_trigger={2}".format(str(xbee_temperature_sensor.min_interval), str(xbee_temperature_sensor.max_interval), str(xbee_temperature_sensor.change_trigger)))
         except Exception as e:
             print("exception in casting the incoming message: {0}".format(str(e)))
     # *** Run the following for each device
@@ -136,19 +140,20 @@ while True:
         except Exception as e:
             print("Cannot send device health: {0}".format(e))
     # **** Run the following for each sensor in each device
-    new_tp = xbee_temperature.measure()
-    if xbee_temperature.should_send(idx, new_tp):
+    new_tp = xbee_temperature_sensor.measure()
+    if xbee_temperature_sensor.should_send(idx, new_tp):
         #  print("Sending data to %s >> %s" % (''.join('{:02x}'.format(x).upper() for x in gps_temp_device.COORD_64_ADDRESS), MESSAGE))
         try:
             xbee.transmit(gps_temp_device.COORD_64_ADDRESS, dumps({'xbee_temp_C': new_tp}))
-            # print("{0} sent successfully".format(dumps({'xbee_temp_C': new_tp})))
-            xbee_temperature.last_sent_measure = new_tp
-            xbee_temperature.idx_last_sent_measure = idx
+            #  print("{0} sent successfully".format(dumps({'xbee_temp_C': new_tp})))
+            xbee_temperature_sensor.last_sent_measure = new_tp
+            xbee_temperature_sensor.idx_last_sent_measure = idx
         except Exception as e:
             print("Transmit exception: %s" % str(e))
         finally:
             gc.collect()
-    # gps_str = uart_gps.measure()
+    #  gps_str = uart_gps.measure()
+    #  print(gps_str)
     # xbee.transmit(gps_temp_device.COORD_64_ADDRESS, dumps({'gps raw': gps_str}))
     idx += 1
     # print("idx={0}, mem_free={1}".format(str(idx), str(gc.mem_free())))
